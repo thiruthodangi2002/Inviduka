e66a43c21e7bf7443fc7a2a1948dd7909c80d01d
Thiru <103431440+thiruthodangi2002@users.noreply.github.com>
2025-08-29 11:54:09 +0530

    Update Contactus.jsx

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -12,10 +12,10 @@ const Contactus = () => {
 
     emailjs
       .sendForm(
-        "service_svc5v1n",        // ✅ Your Service ID
-        "template_np3qpwm",       // ✅ Your Template ID
+        "service_1ptz8dy",        // ✅ Your Service ID
+        "template_q0iyg6l",       // ✅ Your Template ID
         formRef.current,
-        "TClvfqrzHuKZejqA3"       // ✅ Your Public Key
+        "mDwUSlFCrWcATJAqs"       // ✅ Your Public Key
       )
       .then(
         () => {
